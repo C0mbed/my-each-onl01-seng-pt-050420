69070694852654d9ef8e8de771108f6208ad4614
Matt Farncombe <matt@farncombe.co>
2020-05-07 20:26:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection) do |i|
+  if block_given?
+    i = 0
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
